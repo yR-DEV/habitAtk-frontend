2ddd1e38f95b9d1c0bdb0487fc66cd0ed924f71b
yeah-right <claar.zack@gmail.com>
2019-05-07 14:47:54 -0600

    used outside styling and extra bootstrap. Have the things I want and am leaving them there for examples when I start writin the javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    alert('LOADED BREH');
+    // alert('LOADED BREH');
+
+    // const endPoint = 'https://localhost:3000/api/v1/habits';
+    // fetch(endPoint)
+    //     .then(response => response.json())
+    //     .then(responseInJson => console.log(responseInJson))
 });
