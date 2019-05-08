f13bc2330dd30b0e73201e186f90cc186cfbe965
yeah-right <claar.zack@gmail.com>
2019-05-07 19:51:10 -0600

    added animated buttons. working on graph below now

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     // alert('LOADED BREH');
-
     // const endPoint = 'https://localhost:3000/api/v1/habits';
     // fetch(endPoint)
     //     .then(response => response.json())
     //     .then(responseInJson => console.log(responseInJson))
+
+
+    // let titanic = new Titanic({
+    //     hover: false, // auto animated on hover (default true)
+    //     click: true  // auto animated on click/tap (default false)
+    // });
+
+            
+    // <!--Clicking turns this icon on-->
+    // <button onclick="titanic.on(getElementById('checkbox').value)">On</button>
 });
